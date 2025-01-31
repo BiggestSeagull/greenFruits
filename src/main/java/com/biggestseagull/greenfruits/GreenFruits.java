6c65231cb8a45872704828eaeae41f7281cd18a3
BiggestSeagull <biggestseagull@gmail.com>
2025-02-01 01:42:30 +0500

    hello message

--- src/main/java/com/biggestseagull/greenfruits/GreenFruits.java
+++ src/main/java/com/biggestseagull/greenfruits/GreenFruits.java
@@ -1,8 +1,30 @@
 package com.biggestseagull.greenfruits;
 
+import java.util.Scanner;
+
 public class GreenFruits {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Добро пожаловать в систему управления фруктами!");
+        System.out.println(
+                """
+                Главное меню: \n
+                1: Добавить фрукт \n
+                2: Вывести список фруктов \n
+                3: Найти самый дорогой фрукт \n
+                4: Найти фрукты с истекшим сроком годности \n
+                5: Изменить фрукт \n
+                6: Удалить фрукт \n
+                7: Сохранить список фруктов в файл \n
+                8: Загрузить список фруктов из файла \n
+                9: Выйти из программы
+                """
+        );
+        
+        String menuOption = scanner.nextLine();
+        
     }
 }
